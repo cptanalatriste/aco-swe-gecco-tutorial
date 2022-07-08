9839d14b831de2ef285b7d4fa803128cafba1ce1
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-07-08 22:52:24 +0100

    Trying to remove errors on Java repl.it support

--- run.sh
+++
@@ -1,2 +0,0 @@
-javac -classpath libs/isula-2.0.1.jar -d out/production -sourcepath src src/setcovering/antcolony/Main.java
-java -classpath out/production:libs/isula-2.0.1.jar:libs/commons-math3-3.6.1.jar setcovering.antcolony.Main
